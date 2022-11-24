d2b5e2cb33d5416a190cc75975f1f17376f83164
hskwon5170 <hskwon517@gmail.com>
2022-11-24 13:20:14 +0900

    함수타입-옵셔널 파라미터

--- class-note/2_functions.ts
+++ class-note/2_functions.ts
@@ -14,3 +14,8 @@ function sum(a: number, b: number): number {
   return a + b;
 }
 sum(10, 20, 30, 40); // 자바스크립트는 인수를 추가적으로 넣어도 유연하지만, 타입스크립트는 스펙을 정확히 이해하고 제한함
+
+// 함수의 옵셔널 파라미터
+function log(a: string, b?: string) {}
+log("hello world");
+log("hello ts", "abc");
