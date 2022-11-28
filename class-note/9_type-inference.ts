05f55b962b47359d68cd96156efb966bf986b19d
hskwon5170 <hskwon517@gmail.com>
2022-11-29 00:33:53 +0900

    타입 추론

---
+++ class-note/9_type-inference.ts
@@ -0,0 +1,38 @@
+// 타입 추론 기본 1
+var a = 10;
+
+function getB(b = 10) {
+    var c = 'hi'
+    return b+c;
+}
+
+// 타입 추론 기본 2
+// interface Dropdown<T> {
+//     value: T;
+//     title: string;
+// }
+// var shoppingItem: Dropdown<string> = {
+//     value: 'abc',
+//     title: 'hello'
+// }
+
+// 타입 추론 기본 3
+interface Dropdown<T> {
+  value: T;
+  title: string;
+}
+
+interface DetailedDropdown<K> extends Dropdown<K>{
+    tag: K;
+    description: string;
+}
+
+var detailedItem: DetailedDropdown<number> = {
+    title: 'abc',
+    description: 'ab',
+    value: 'ab',
+    tag: 'a'
+}
+
+//  Best Common Type
+var arr = [1, 2, 3];
