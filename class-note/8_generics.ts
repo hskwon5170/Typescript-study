e480a8e71f414810fd60cc3cb4756b1867c72c9e
hskwon5170 <hskwon517@gmail.com>
2022-11-28 17:50:51 +0900

    keyof로 제네릭 타입 제한하기

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -67,11 +67,37 @@ const obj: Dropdown<string> = { value: 'abc', selected: false }
 
 
 // 제네릭의 타입 제한 (타입 힌트를 추가적으로 제공)
-function logTextLength<T>(text: T[]): T[] {
-  console.log(text.length)
-  text.forEach((text) => {
-    console.log(text)
-  })
-  return text
+// function logTextLength<T>(text: T[]): T[] {
+//   console.log(text.length)
+//   text.forEach((text) => {
+//     console.log(text)
+//   })
+//   return text
+// }
+// logTextLength<string>(['hi', 'abc'])
+
+// 제네릭의 타입 제한 2 - 정의된 타입 이용하기
+interface LengthType {
+  length: number;
 }
-logTextLength(['hi','abc'])
+
+function logTextLength<T extends LengthType>(text: T): T {
+  text.length;
+  return text;
+}
+logTextLength(10)
+
+// 제네릭의 타입 제한 3 - keyof
+interface ShoppingItem {
+  name: string;
+  price: number;
+  stock: number;
+}
+
+// shoppingItem의 키값 중 하나가 제네릭이 된다 (제네릭에 들어올수있는 옵션을 제한)
+function getShoppingItemOption<T extends keyof ShoppingItem>(itemOption: T): T {
+  return itemOption
+  
+}
+// getShoppingItemOption(10)
+getShoppingItemOption("name")
